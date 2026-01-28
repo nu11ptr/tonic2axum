b7f800f85b8b0f0e87f0ac9ebbc572451a192ca5
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2026-01-27 22:25:10 -0500

    Refactor Builder

--- examples/simple/build.rs
+++ examples/simple/build.rs
@@ -1,28 +1,20 @@
-use std::{error::Error, path::PathBuf};
+use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let fds_path = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("fds.bin");
+    // 1. tonic_prost_build: Configure the Builder
+    let tonic_builder = tonic_prost_build::configure().build_client(false);
 
-    // 1. prost_build: Configure to save file descriptor set to a file, compile the proto files and read the file descriptor set from the file.
-    let mut config = prost_build::Config::new();
-    config.file_descriptor_set_path(&fds_path);
-    let fds = config.load_fds(&["proto/hello/v1/hello.proto"], &["proto"])?;
-    let bytes = std::fs::read(&fds_path)?;
+    // 2. tonic2axum_build: Configure the Builder
+    let mut builder = tonic2axum_build::Builder::new().tonic_builder(tonic_builder);
 
-    // 2. pbjson_build: Implement Serialization and Deserialization for the messages
+    // 3. tonic2axum_build: Compile the proto files and return the file descriptor set and its raw bytes.
+    let (fds, fds_bytes) = builder.compile_protos(&["proto/hello/v1/hello.proto"], &["proto"])?;
+
+    // 4. pbjson_build: Implement Serialization and Deserialization for the messages
     pbjson_build::Builder::new()
-        .register_descriptors(&bytes)?
+        .register_descriptors(&fds_bytes)?
         .build(&[".hello.v1"])?;
 
-    // 3. tonic_prost_build: Configure the Builder
-    let prost_builder = tonic_prost_build::configure().build_client(false);
-
-    // 4. tonic2axum_build: We wrap the tonic_prost_build service generator in our builder, and use our service generator with prost_build.
-    let t2a_builder = tonic2axum_build::Builder::new(prost_builder.service_generator(), bytes);
-    config.service_generator(t2a_builder.into_service_generator()?);
-
-    // 5. prost_build: Generate the Rust files
-    config.compile_fds(fds)?;
-
-    Ok(())
+    // 5. tonic2axum_build: Generate the code (Prost, Tonic, and Axum)
+    builder.generate_code(fds, fds_bytes)
 }
--- tonic2axum-build/src/builder.rs
+++ tonic2axum-build/src/builder.rs
@@ -1,11 +1,15 @@
 use std::collections::HashMap;
 use std::error::Error;
+use std::path::{Path, PathBuf};
 
 use flexstr::LocalStr;
 use flexstr::str::LocalStrRef;
 use prost_build::ServiceGenerator;
+use prost_reflect::prost_types::FileDescriptorSet;
 
 use crate::codegen::Generator;
+
+const DEFAULT_FDS_FILE_NAME: &str = "fds.bin";
 
 pub enum StateType {
     Custom(LocalStr),
@@ -13,18 +17,25 @@ pub enum StateType {
 }
 
 pub struct Builder {
-    service_generator: Box<dyn ServiceGenerator>,
-    bytes: Vec<u8>,
+    fds_path: Option<PathBuf>,
     state_types: HashMap<LocalStr, StateType>,
+    prost_config: Option<prost_build::Config>,
+    tonic_builder: Option<tonic_prost_build::Builder>,
 }
 
 impl Builder {
-    pub fn new(service_generator: Box<dyn ServiceGenerator>, bytes: Vec<u8>) -> Self {
+    pub fn new() -> Self {
         Self {
-            service_generator,
-            bytes,
+            fds_path: None,
             state_types: HashMap::new(),
+            prost_config: None,
+            tonic_builder: None,
         }
+    }
+
+    pub fn file_descriptor_set_path(mut self, path: impl Into<PathBuf>) -> Self {
+        self.fds_path = Some(path.into());
+        self
     }
 
     pub fn custom_state_type(
@@ -46,11 +57,75 @@ impl Builder {
         self
     }
 
-    pub fn into_service_generator(self) -> Result<Box<dyn ServiceGenerator>, Box<dyn Error>> {
+    pub fn prost_config(mut self, config: prost_build::Config) -> Self {
+        self.prost_config = Some(config);
+        self
+    }
+
+    pub fn tonic_builder(mut self, builder: tonic_prost_build::Builder) -> Self {
+        self.tonic_builder = Some(builder);
+        self
+    }
+
+    pub fn compile_protos(
+        &mut self,
+        protos: &[impl AsRef<Path>],
+        includes: &[impl AsRef<Path>],
+    ) -> Result<(FileDescriptorSet, Vec<u8>), Box<dyn Error>> {
+        if self.fds_path.is_none() {
+            let mut fds_path = match std::env::var("OUT_DIR") {
+                Ok(out_dir) => PathBuf::from(out_dir),
+                Err(_) => PathBuf::from("."),
+            };
+            fds_path.push(DEFAULT_FDS_FILE_NAME);
+            self.fds_path = Some(fds_path);
+        }
+
+        if self.prost_config.is_none() {
+            self.prost_config = Some(prost_build::Config::new());
+        }
+
+        let fds_path = self.fds_path.as_ref().unwrap();
+        let prost_config = self.prost_config.as_mut().unwrap();
+
+        prost_config.file_descriptor_set_path(fds_path);
+        let fds = prost_config.load_fds(protos, includes)?;
+        let bytes = std::fs::read(fds_path)?;
+        Ok((fds, bytes))
+    }
+
+    pub fn generate_code(
+        mut self,
+        fds: FileDescriptorSet,
+        fds_bytes: Vec<u8>,
+    ) -> Result<(), Box<dyn Error>> {
+        if self.prost_config.is_none() {
+            self.prost_config = Some(prost_build::Config::new());
+        }
+
+        if self.tonic_builder.is_none() {
+            self.tonic_builder = Some(tonic_prost_build::configure());
+        }
+
+        let prost_config = self.prost_config.as_mut().unwrap();
+        let tonic_builder = self.tonic_builder.unwrap();
+
+        let service_generator =
+            Self::make_service_generator(tonic_builder, fds_bytes, self.state_types)?;
+        prost_config.service_generator(service_generator);
+        prost_config.compile_fds(fds)?;
+        Ok(())
+    }
+
+    fn make_service_generator(
+        tonic_builder: tonic_prost_build::Builder,
+        fds_bytes: Vec<u8>,
+        state_types: HashMap<LocalStr, StateType>,
+    ) -> Result<Box<dyn ServiceGenerator>, Box<dyn Error>> {
         Ok(Box::new(Generator::new(
-            self.service_generator,
-            self.bytes,
-            self.state_types,
+            tonic_builder.service_generator(),
+            fds_bytes,
+            state_types,
         )?))
     }
 }
