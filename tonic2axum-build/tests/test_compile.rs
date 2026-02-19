e8503484e7975efda75a13b8cf0575008ae3c1e1
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2026-02-18 21:08:29 -0500

    Gate test compiles so they don't run with cleanup-markdown

--- tonic2axum-build/tests/test_compile.rs
+++ tonic2axum-build/tests/test_compile.rs
@@ -1,40 +1,43 @@
-use tempfile::tempdir;
-use tonic2axum_build::{Builder, OpenApiSecurity, ProstConfig};
+#[cfg(not(feature = "cleanup-markdown"))]
+mod test_compile {
+    use tempfile::tempdir;
+    use tonic2axum_build::{Builder, OpenApiSecurity, ProstConfig};
 
-#[test]
-fn test_compile() {
-    let dir = tempdir().unwrap();
+    #[test]
+    fn test_compile() {
+        let dir = tempdir().unwrap();
 
-    let mut config = ProstConfig::new();
-    config
-        .out_dir(dir.path())
-        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
-    Builder::new()
-        .prost_config(config)
-        .compile(&["tests/proto/test/v1/test.proto"], &["tests/proto"])
-        .unwrap();
+        let mut config = ProstConfig::new();
+        config
+            .out_dir(dir.path())
+            .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+        Builder::new()
+            .prost_config(config)
+            .compile(&["tests/proto/test/v1/test.proto"], &["tests/proto"])
+            .unwrap();
 
-    let actual = std::fs::read_to_string(dir.path().join("test.v1.rs")).unwrap();
-    let expected = std::fs::read_to_string("tests/testdata/test.v1.rs").unwrap();
-    assert_eq!(actual, expected);
-}
+        let actual = std::fs::read_to_string(dir.path().join("test.v1.rs")).unwrap();
+        let expected = std::fs::read_to_string("tests/testdata/test.v1.rs").unwrap();
+        assert_eq!(actual, expected);
+    }
 
-#[test]
-fn test_compile_with_openapi_security() {
-    let dir = tempdir().unwrap();
+    #[test]
+    fn test_compile_with_openapi_security() {
+        let dir = tempdir().unwrap();
 
-    let mut config = ProstConfig::new();
-    config
-        .out_dir(dir.path())
-        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
-    Builder::new()
-        .prost_config(config)
-        .generate_openapi(true)
-        .openapi_security(OpenApiSecurity::AllServices("Bearer"))
-        .compile(&["tests/proto/test/v1/test.proto"], &["tests/proto"])
-        .unwrap();
+        let mut config = ProstConfig::new();
+        config
+            .out_dir(dir.path())
+            .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+        Builder::new()
+            .prost_config(config)
+            .generate_openapi(true)
+            .openapi_security(OpenApiSecurity::AllServices("Bearer"))
+            .compile(&["tests/proto/test/v1/test.proto"], &["tests/proto"])
+            .unwrap();
 
-    let actual = std::fs::read_to_string(dir.path().join("test.v1.rs")).unwrap();
-    let expected = std::fs::read_to_string("tests/testdata/openapi/test.v1.rs").unwrap();
-    assert_eq!(actual, expected);
+        let actual = std::fs::read_to_string(dir.path().join("test.v1.rs")).unwrap();
+        let expected = std::fs::read_to_string("tests/testdata/openapi/test.v1.rs").unwrap();
+        assert_eq!(actual, expected);
+    }
 }
